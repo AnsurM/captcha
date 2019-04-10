a0f8cc14757822f985c79aa178eff455a691202b
AnsurM <ansurmehdi@gmail.com>
2019-04-10 18:38:59 +0500

    Updates

---
+++ precache-manifest.6f9fba7ca9ad58dd897d7964c1191cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/captcha/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de26e3666a615cf345e1",
+    "url": "/captcha/static/js/runtime~main.65cf6072.js"
+  },
+  {
+    "revision": "550cfa6290c37c3c980f",
+    "url": "/captcha/static/js/main.4414f174.chunk.js"
+  },
+  {
+    "revision": "fba78ac67d580bacbc27",
+    "url": "/captcha/static/js/2.1f6739a1.chunk.js"
+  },
+  {
+    "revision": "550cfa6290c37c3c980f",
+    "url": "/captcha/static/css/main.53e7df4a.chunk.css"
+  },
+  {
+    "revision": "64e1427d0b6beb7ed6c99bd76f2918c1",
+    "url": "/captcha/index.html"
+  }
+];
